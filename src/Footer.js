282164ac386158b8c0fb7746cb4bd05fdf6e94e2
Sourin <janasourin2017@gmail.com>
2023-12-14 23:09:31 +0530

    add project all files

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.css';
+
+function Footer() {
+    return(
+        <footer className="footer">
+            <p></p>
+        </footer>
+    );
+}
+export default Footer;
